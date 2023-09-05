323b4b2b9facef0711c9ee1a079d4c8820233c33
chandra <candrasachio@gmail.com>
2023-09-05 22:40:07 +0700

    change endpoint current meeting

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::group(['prefix' => 'v1/guest'], function ($router) {
     $router->get('/list/room', [LandingpageController::class, 'listRoom'])->name('landingpage.list.room');
     $router->get('/list/current-meeting/{room_id}', [LandingpageController::class, 'listCurrentRent'])->name('landingpage.current.event');
 });
-Route::group(['prefix' => 'v1/rent'], function ($router) {
+Route::group(['prefix' => 'v1/room'], function ($router) {
     $router->get('/check-meeting/{room_id}', [RentController::class, 'checkMeeting'])->name('guest.checkMeeting');
 });
 Route::group(['middleware' => 'auth:api'], function ($router) {
